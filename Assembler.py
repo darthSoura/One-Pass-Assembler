6df75662c928a47fcdb2d5bc2b55f7829b8bc186
Souradeep Banerjee <ash007online@gmail.com>
2022-04-22 14:12:21 +0530

    Label Dictionary created

--- Assembler.py
+++ Assembler.py
@@ -25,15 +25,22 @@ for line in data:
         continue
     
     if len(words) == 3: # add label and address
-        labels[words[0]] = loc
+        labels[words[0]] = str.upper(loc)
     
-    if len(words) == 1:
+    if len(words) == 1: # for RSUB
         loc = str(hex(int(loc, 16) + 3)[2:])
         continue
     
     # Handle the case of buffer and array lengths
+    if words[-2] == 'RESW':
+        size = int(words[-1])*3
+        loc = str(hex(int(loc, 16) + size)[2:]) 
     
-    if words[-2] != 'START' and words[-2] != 'BYTE':
+    elif words[-2] == 'RESB':
+        size = int(words[-1])
+        loc = str(hex(int(loc, 16) + size)[2:])
+    
+    elif words[-2] != 'START' and words[-2] != 'BYTE':
         loc = str(hex(int(loc, 16) + 3)[2:])
     
     elif words[-2] == 'START':
