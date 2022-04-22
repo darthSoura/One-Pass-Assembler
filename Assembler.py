234579674140306c4db773a8a99a25408f86ff4c
Souradeep Banerjee <ash007online@gmail.com>
2022-04-22 15:31:14 +0530

    Object Code Generation

--- Assembler.py
+++ Assembler.py
@@ -9,11 +9,10 @@ with open('input.txt', 'r') as file:
     data = file.readlines()
     
 loc = data[0].split()[-1]
-print(loc)
 
 objCode = {}
 labels = {}
-backRef = {}
+forRef = {}
 
 # def hex_add(addr, bytes):
     
@@ -25,13 +24,21 @@ for line in data:
         continue
     
     if len(words) == 3: # add label and address
+        
+        if words[0] in forRef:
+            pass
         labels[words[0]] = str.upper(loc)
     
     if len(words) == 1: # for RSUB
+        objCode[str.upper(loc)] = OPTAB[words[-1]] + '0000'
         loc = str(hex(int(loc, 16) + 3)[2:])
+        
         continue
     
     # Handle the case of buffer and array lengths
+    if words[-2] == 'START' or words[-2] == 'END':
+        continue
+    
     if words[-2] == 'RESW':
         size = int(words[-1])*3
         loc = str(hex(int(loc, 16) + size)[2:]) 
@@ -40,16 +47,34 @@ for line in data:
         size = int(words[-1])
         loc = str(hex(int(loc, 16) + size)[2:])
     
+    
     elif words[-2] != 'START' and words[-2] != 'BYTE':
+        
+        if words[-2] == 'WORD':
+            objCode[str.upper(loc)] = str.upper('000000' + str(hex(int(words[-1])))[2:])[-6:]
+    
+        else:
+            if words[-1] in labels:
+                objCode[str.upper(loc)] = OPTAB[words[-2]] + labels[words[-1]]
+                
+            else:
+                if words[-1] in forRef:
+                    forRef[words[-1]].append(loc)
+                else:
+                    forRef[words[-1]] = [loc]
+                objCode[str.upper(loc)] = OPTAB[words[-2]] + '0000'
+            
+            
         loc = str(hex(int(loc, 16) + 3)[2:])
-    
-    elif words[-2] == 'START':
-        continue
-    
+        
     elif words[-2] == 'BYTE':
         if words[-3] == 'EOF':
+            objCode[str.upper(loc)] = '454F46'
             loc = str(hex(int(loc, 16) + 3)[2:])
         else:
+            objCode[str.upper(loc)] = str.upper(words[-1][-3:-1])
             loc = str(hex(int(loc, 16) + 1)[2:])
 
-print(labels)
+# print(labels)
+
+print("Object Code: ", objCode)
